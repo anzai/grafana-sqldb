966096a3dc272c6b31a581b4ad95b7521b6ac841
Nozomi Anzai <anzai@sraoss.co.jp>
2016-06-13 16:40:55 +0900

    Modified

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,41 +5,50 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-typescript');
 
   grunt.initConfig({
-    clean: ["ts", "dist"],
+    clean: ['src_gen', 'dist'],
+
+    copy: {
+      src_js: {
+        expand: true,
+        cwd: 'src',
+        src: ['**/*.js', '**/*.ts', '**/*.d.ts'],
+        dest: 'src_gen/'
+      },
+
+      dist_js: {
+        expand: true,
+        flatten: true,
+        cwd: 'src_gen/public/',
+        src: ['*.js', '*.ts', '*.d.ts', '*.js.map'],
+        dest: 'dist/'
+      },
+      dist_html: {
+        expand: true,
+        flatten: true,
+        cwd: 'src/public/partials',
+        src: ['*.html'],
+        dest: 'dist/partials/'
+      },
+      dist_statics: {
+        expand: true,
+        src: ['plugin.json', 'LICENSE'],
+        dest: 'dist/'
+      }
+    },
 
     typescript: {
       build: {
-        src: ['src/**/*.ts'],
-        out: "ts",
+        src: ['src_gen/**/*.ts', '!**/*.d.ts'],
+        out: 'dist',
         options: {
-          module: 'system', //or commonjs
-          target: 'es5', //or es3
+          module: 'system',
+          target: 'es5',
           declaration: true,
           emitDecoratorMetadata: true,
           experimentalDecorators: true,
           sourceMap: true,
           noImplicitAny: false,
         }
-      }
-    },
-
-    copy: {
-      typescript: {
-        expand: true,
-        cwd: 'src',
-        src: ['**/*.ts', '**/*.js'],
-        dest: 'ts/'
-      },
-      html: {
-        expand: true,
-        flatten: true,
-        src: ['src/public/partials/**'],
-        dest: 'dist/partials/'
-      },
-      statics: {
-        expand: true,
-        src: ['plugin.json', 'LICENSE'],
-        dest: 'dist/'
       }
     },
 
@@ -51,33 +60,23 @@ module.exports = function(grunt) {
       },
       js: {
         files: [{
-          cwd: 'src/public',
           expand: true,
+          cwd: 'src_gen',
           src: ['**/*.js'],
-          dest: 'dist',
-          ext:'.js'
-        }]
-      },
-      ts: {
-        files: [{
-          cwd: 'ts',
-          expand: true,
-          src: ['**/*.js'],
-          dest: 'dist',
+          dest: 'src_gen/',
           ext:'.js'
         }]
       }
     }
-
   });
 
   grunt.registerTask('default', [
     'clean',
-    'copy:typescript',
+    'copy:src_js',
+    //'babel:js',
     'typescript:build',
-    //'copy:javascript',
-    'copy:html',
-    'copy:statics',
-    'babel'
+    'copy:dist_js',
+    'copy:dist_html',
+    'copy:dist_statics'
   ]);
 };
